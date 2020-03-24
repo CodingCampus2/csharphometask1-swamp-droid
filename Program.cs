45a8b0c6af176c0058b5270dd809fbd153285a82
Mykhailo Danilchenko <mykhailo.danilchenko@ubisoft.com>
2020-03-24 19:42:39 +0200

    add answer output

--- Program.cs
+++ Program.cs
@@ -12,8 +12,18 @@ namespace HomeworkTemplate
                 // Your solution goes here
                 // You can get all needed inputs from task.[Property]
                 // Good luck!
+                float population = Single.Parse(task.City.Population);
+                float sickPercentage = Single.Parse(task.City.SickPercentage);
+                float killProbability = Single.Parse(task.Virus.KillProbability);
 
-                return "My answer";
+                float sickValue = population * sickPercentage;
+                float deadValue = sickValue * killProbability;
+
+                string answer = "There are " + Math.Truncate(sickValue) + " people sick with " +
+                    task.Virus.Name + " in the city of " + task.City.Name + 
+                    ", " + Math.Truncate(deadValue) + " of which died";
+
+                return answer;
             };
 
             Task1.CheckSolver(TaskSolver);
